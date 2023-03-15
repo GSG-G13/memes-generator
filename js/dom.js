6b69e8ec65a351d00b4a8e1c4b43763dcbbe3853
momenmarwan <momenalgaga82@gmail.com>
2023-03-15 14:06:40 +0200

    make a createcard function closes 17

--- js/dom.js
+++ js/dom.js
@@ -1,5 +1,6 @@
 const addListener = (selector, eventName, callback) => {
-  document.querySelector(selector).addEventListener(eventName, callback);
+  if (selector !== null)
+    document.querySelector(selector).addEventListener(eventName, callback);
 };
 
 const getElementById = (id) => {
@@ -27,6 +28,56 @@ const textJoke = document.querySelector("header .random-joke");
 const changeValue = (element, text) => {
   element.textContent = text;
 };
+
+const createHtmlElement = (el, className, id) => {
+  const ele = document.createElement(el);
+  if (className) {
+    ele.className = className;
+  }
+  if (id) {
+    ele.id = id;
+  }
+  return ele;
+};
+
+const appendChildrn = (parent, ...children) => {
+  children.forEach((child) => {
+    parent.appendChild(child);
+  });
+};
+
+const createCard = (obj , cb) => {
+  const divCard = createHtmlElement("div", "card");
+  const imageContainer = createHtmlElement("div", "image-container");
+  const img = createHtmlElement("img");
+  img.setAttribute("src", obj.url);
+  imageContainer.appendChild(img);
+  const memeCaption = createHtmlElement("p", "meme-caption");
+  memeCaption.textContent = obj.name;
+  const addBtn = createHtmlElement("button", "add-button");
+  addBtn.textContent = "add meme";
+  addBtn.addEventListener("click" , cb)
+  appendChildrn(divCard, imageContainer, memeCaption, addBtn);
+  return divCard;
+};
+
+const cards = document.querySelector(".cards");
+
+const obj = {
+  id: "119139145",
+  name: "Blank Nut Button",
+  url: "https://i.imgflip.com/1yxkcp.jpg",
+  width: 600,
+  height: 446,
+  box_count: 2,
+  captions: 0,
+};
+const cb = () => {
+    console.log("Hello meme")
+}
+
+const card = createCard(obj , cb);
+if (cards != null) cards.appendChild(card);
 
 addListener("#top-text", "input", () => {
   changeValue(topTextElement, topText.value);
--- js/logic.js
+++ js/logic.js
@@ -1,16 +1,18 @@
-const fetch = (url , cb) => {
-    let xhr = new XMLHttpRequest()
-    xhr.onreadystatechange = () => {
-        if(xhr.readyState == 4 && xhr.status ==200){
-            let response = JSON.parse(xhr.responseText)
-            cb(response)
-        }
+const fetch = (url, cb) => {
+  let xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = () => {
+    if (xhr.readyState == 4 && xhr.status == 200) {
+      let response = JSON.parse(xhr.responseText);
+      cb(response);
     }
-    xhr.open("GET" , url , true)
-    xhr.send()
-}
+  };
+  xhr.open("GET", url, true);
+  xhr.send();
+};
 
-fetch("https://geek-jokes.sameerkumar.website/api?format=json" , (response) => {
-    let joke = response.joke
-    textJoke.textContent = joke
-})
+fetch("https://geek-jokes.sameerkumar.website/api?format=json", (response) => {
+  let joke = response.joke;
+  textJoke.textContent = joke;
+});
+
+
