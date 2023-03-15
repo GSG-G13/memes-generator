c19826f5f8b6f04b459f1fab9eed38637de0250c
momenmarwan <momenalgaga82@gmail.com>
2023-03-15 16:46:10 +0200

    fix deploy

--- js/logic.js
+++ js/logic.js
@@ -19,7 +19,7 @@ const render = (res) => {
   res.forEach((obj) => {
     const card = createCard(obj, () => {
       const url = `meme.html?id=${obj.id}`;
-      window.location.href(url);
+      window.location.assign(url);
     });
     if (cards != null) cards.appendChild(card);
   });
@@ -54,75 +54,73 @@ fetch(`https://api.imgflip.com/get_memes`, (response) => {
 
 const testArray = [
   {
-    "id": "181913649",
+    id: "181913649",
     name: "Drake Hotline Bling",
-    "url": "https://i.imgflip.com/30b1gx.jpg",
-    "width": 1200,
-    "height": 1200,
-    "box_count": 2,
-    "captions": 0
-    },
-    {
-    "id": "87743020",
+    url: "https://i.imgflip.com/30b1gx.jpg",
+    width: 1200,
+    height: 1200,
+    box_count: 2,
+    captions: 0,
+  },
+  {
+    id: "87743020",
     name: "Two Buttons",
-    "url": "https://i.imgflip.com/1g8my4.jpg",
-    "width": 600,
-    "height": 908,
-    "box_count": 3,
-    "captions": 0
-    },
-    {
-    "id": "112126428",
+    url: "https://i.imgflip.com/1g8my4.jpg",
+    width: 600,
+    height: 908,
+    box_count: 3,
+    captions: 0,
+  },
+  {
+    id: "112126428",
     name: "Distracted Boyfriend",
-    "url": "https://i.imgflip.com/1ur9b0.jpg",
-    "width": 1200,
-    "height": 800,
-    "box_count": 3,
-    "captions": 0
-    },
-    {
-    "id": "131087935",
+    url: "https://i.imgflip.com/1ur9b0.jpg",
+    width: 1200,
+    height: 800,
+    box_count: 3,
+    captions: 0,
+  },
+  {
+    id: "131087935",
     name: "Running Away Balloon",
-    "url": "https://i.imgflip.com/261o3j.jpg",
-    "width": 761,
-    "height": 1024,
-    "box_count": 5,
-    "captions": 0
-    },
-    {
-    "id": "217743513",
+    url: "https://i.imgflip.com/261o3j.jpg",
+    width: 761,
+    height: 1024,
+    box_count: 5,
+    captions: 0,
+  },
+  {
+    id: "217743513",
     name: "Running 25 Cards",
-    "url": "https://i.imgflip.com/3lmzyx.jpg",
-    "width": 500,
-    "height": 494,
-    "box_count": 2,
-    "captions": 0
-    },
-    {
-    "id": "124822590",
+    url: "https://i.imgflip.com/3lmzyx.jpg",
+    width: 500,
+    height: 494,
+    box_count: 2,
+    captions: 0,
+  },
+  {
+    id: "124822590",
     name: "Running 12 Off Ramp",
-    "url": "https://i.imgflip.com/22bdq6.jpg",
-    "width": 804,
-    "height": 767,
-    "box_count": 3,
-    "captions": 0
-    }
-]
-const searchMeme = (array , value) => {
+    url: "https://i.imgflip.com/22bdq6.jpg",
+    width: 804,
+    height: 767,
+    box_count: 3,
+    captions: 0,
+  },
+];
+const searchMeme = (array, value) => {
   return array.filter((object) => {
-    if(object.name.startsWith(value) ){
-      return object
+    if (object.name.startsWith(value)) {
+      return object;
     }
-  })
-}
+  });
+};
 
-const searchInput = document.querySelector(".search-feild")
-console.log()
+const searchInput = document.querySelector(".search-feild");
+console.log();
 
-searchInput.addEventListener("input" , (e) => {
-  console.log(e.target.value)
-  let array = searchMeme(testArray , searchInput.value)
-  console.log(array)
-})
-
-
+searchInput.addEventListener("input", (e) => {
+  console.log(e.target.value);
+  let array = searchMeme(testArray, searchInput.value);
+  console.log(array);
+});
