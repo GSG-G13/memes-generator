7d8ac2f8ef524a74356127408d86248c619f6c6c
Ahmed Izz Murtaja <56529633+ahmedmurtaja@users.noreply.github.com>
2023-03-15 13:18:28 +0200

    fetch api Closes #14

--- js/logic.js
+++ js/logic.js
@@ -1,16 +1,21 @@
-const fetch = (url , cb) => {
-    let xhr = new XMLHttpRequest()
-    xhr.onreadystatechange = () => {
-        if(xhr.readyState == 4 && xhr.status ==200){
-            let response = JSON.parse(xhr.responseText)
-            cb(response)
-        }
+const fetch = (url, cb) => {
+  const xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = () => {
+    if (xhr.readyState == 4 && xhr.status == 200) {
+      const response = JSON.parse(xhr.responseText);
+      cb(response);
     }
-    xhr.open("GET" , url , true)
-    xhr.send()
-}
+  };
+  xhr.open("GET", url, true);
+  xhr.send();
+};
 
-fetch("https://geek-jokes.sameerkumar.website/api?format=json" , (response) => {
-    let joke = response.joke
-    textJoke.textContent = joke
-})
+fetch("https://geek-jokes.sameerkumar.website/api?format=json", (response) => {
+  const joke = response.joke;
+  textJoke.textContent = joke;
+});
+
+fetch("https://api.imgflip.com/get_memes", (response) => {
+  const res = response.data.memes;
+  console.log(res);
+});
